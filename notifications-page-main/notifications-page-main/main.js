f65f826aa4133a375436252e9af83f12b68ed67e
João Paulo Moreira <jpmoreira195@outlook.com>
2022-10-03 17:54:01 -0300

    index, style and main update

---
+++ notifications-page-main/notifications-page-main/main.js
@@ -0,0 +1,9 @@
+function openModal(){
+   const openModal = document.querySelector(".modal")
+   openModal.getElementsByClassName.display = "block"
+}
+
+function closeModal(){
+   const closeModal = document.querySelector(".modal")
+   closeModal.getElementsByClassName.display = "none"
+}
