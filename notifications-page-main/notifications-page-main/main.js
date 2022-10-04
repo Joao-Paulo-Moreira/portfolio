838e4704d96bec9dc4d208d20455f5d4bdf8855d
João Paulo Moreira <jpmoreira195@outlook.com>
2022-10-04 18:46:51 -0300

    update

--- notifications-page-main/notifications-page-main/main.js
+++ notifications-page-main/notifications-page-main/main.js
@@ -1,9 +1,71 @@
 function openModal(){
-   const openModal = document.querySelector(".modal")
-   openModal.getElementsByClassName.display = "block"
+   const openModal = document.querySelector(".Modal")
+   openModal.style.display = "block"
+}
+
+function openSecondModal(){
+   const openModal = document.querySelector(".secondModal")
+   openModal.style.display = "block"
+}
+
+function openThirdModal(){
+   const openModal = document.querySelector(".thirdModal")
+   openModal.style.display = "block"
+}
+
+function openForthModal() {
+   const openModal = document.querySelector(".forthModal")
+   openModal.style.display = "block"
+}
+
+function openFiveModal() {
+   const openModal = document.querySelector(".fiveModal")
+   openModal.style.display = "block"
+}
+
+function openSixModal() {
+   const openModal = document.querySelector(".sixModal")
+   openModal.style.display = "block"
+}
+
+function openSevenModal() {
+   const openModal = document.querySelector(".sevenModal")
+   openModal.style.display = "block"
+}
+
+
+
+function closeModal(){
+   const closeModal = document.querySelector(".Modal")
+   closeModal.style.display = "none"
 }
 
 function closeModal(){
-   const closeModal = document.querySelector(".modal")
-   closeModal.getElementsByClassName.display = "none"
+   const closeModal = document.querySelector(".secondModal")
+   closeModal.style.display = "none"
+}
+
+function closeModal() {
+   const closeModal = document.querySelector(".thirdModal")
+   closeModal.style.display = "none"
+}
+
+function closeModal() {
+   const closeModal = document.querySelector(".forthModal")
+   closeModal.style.display = "none"
+}
+
+function closeModal() {
+   const closeModal = document.querySelector(".fiveModal")
+   closeModal.style.display = "none"
+}
+
+function closeModal() {
+   const closeModal = document.querySelector(".sixModal")
+   closeModal.style.display = "none"
+}
+
+function closeModal() {
+   const closeModal = document.querySelector(".sevenModal")
+   closeModal.style.display = "none"
 }
